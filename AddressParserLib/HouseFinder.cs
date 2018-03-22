2d3a9a9cd8e7dccf9aa7a181a7c0ee8b5158f01d
Kirill.Sizov <Kirill.Sizov@TERMINATOR>
2018-03-22 17:53:27 +0300

    Fixed House Cunc Parser

---
+++ AddressParserLib/HouseFinder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AddressParserLib
+{
+    public class HouseFinder
+    {
+        private static HouseFinder hs;
+        private Regex houseReg;
+        private static readonly string HOUSE_PATTERN = @"([0-9]+)|((д\.|дом).*[0-9]+.*)";
+        private static readonly float FIND_RATIO = 2 / 3f;
+
+        public HouseFinder()
+        {
+            houseReg = new Regex(HOUSE_PATTERN, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        }
+
+
+        public static HouseFinder GetInstance()
+        {
+            if (hs != null)
+                return hs;
+            return hs = new HouseFinder();
+        }
+
+        public string Parse(string source)
+        {
+            MatchCollection matchCollection = houseReg.Matches(source);
+
+            if (matchCollection.Count == 0)
+            {
+                return null;
+            }
+            else if (matchCollection.Count == 1)
+            {
+                if (matchCollection[0].Index > source.Length * 3 / 4)
+                    return source.Substring(matchCollection[0].Index);
+                return matchCollection[0].Value;
+            }
+            else
+            {
+                List<Int32> indexes = new List<Int32>();
+                foreach (Match match in matchCollection)
+                {
+                    indexes.Add(match.Index);
+                }
+                indexes.Sort();
+
+                int minIndex = indexes[indexes.Count - 1];
+
+                foreach (var index in indexes)
+                {
+                    if ((index < minIndex) && (index > source.Length * FIND_RATIO))
+                        minIndex = index;
+                }
+
+                return source.Substring(minIndex);
+            }
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using AddressParserLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HouseFinder hFinder = HouseFinder.GetInstance();
+            Console.WriteLine(hFinder.Parse("361325, КАБАРДИНО - БАЛКАРСКАЯ РЕСП, УРВАНСКИЙ Р - Н, СТАРЫЙ ЧЕРЕК С, ПОЧТОВАЯ УЛ, ДОМ № 10"));
+
+
+            Console.ReadKey();
+           
+        }
+    }
+}
